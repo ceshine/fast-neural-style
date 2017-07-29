fe8473b37125d08426292f3653df24fc8d05cba7
CeShine Lee <shuanck@gmail.com>
2017-07-29 14:02:18 +0800

    Create videos

--- utils.py
+++ utils.py
@@ -1,8 +1,5 @@
-import os
-
 import numpy as np
-import torch
-from torch.autograd import Variable
+from torchvision import transforms
 
 
 def gram_matrix(y):
@@ -11,3 +8,20 @@ def gram_matrix(y):
     features_t = features.transpose(1, 2)
     gram = features.bmm(features_t) / (ch * h * w) * 100
     return gram
+
+
+def tensor_normalizer():
+    return transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225])
+
+
+def recover_image(img):
+    return (
+        (
+            img *
+            np.array([0.229, 0.224, 0.225]).reshape((1, 3, 1, 1)) +
+            np.array([0.485, 0.456, 0.406]).reshape((1, 3, 1, 1))
+        ).transpose(0, 2, 3, 1) *
+        255.
+    ).clip(0, 255).astype(np.uint8)
