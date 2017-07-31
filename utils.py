06c25865d436dedff1e3a5f11aee4985e9f3a0ab
CeShine Lee <shuanck@gmail.com>
2017-07-31 10:21:02 +0800

    Disable scaling of gram matrix in utils and update notebook example

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def gram_matrix(y):
     (b, ch, h, w) = y.size()
     features = y.view(b, ch, w * h)
     features_t = features.transpose(1, 2)
-    gram = features.bmm(features_t) / (ch * h * w) * 100
+    gram = features.bmm(features_t) / (ch * h * w)
     return gram
 
 
