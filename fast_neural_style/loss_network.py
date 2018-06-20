1b7a8e08f01d2d0bee224b7fafc58c449927fcb3
CeShine Lee <shuanck@gmail.com>
2018-06-20 10:38:09 +0800

    Put loss network in a sepearate module

---
+++ fast_neural_style/loss_network.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+
+import torch
+
+LossOutput = namedtuple(
+    "LossOutput", ["relu1_2", "relu2_2", "relu3_3", "relu4_3"])
+
+
+class LossNetwork(torch.nn.Module):
+    """Reference:
+        https://discuss.pytorch.org/t/how-to-extract-features-of-an-image-from-a-trained-model/119/3
+    """
+
+    def __init__(self, vgg_model):
+        super(LossNetwork, self).__init__()
+        self.vgg_layers = vgg_model.features
+        self.layer_name_mapping = {
+            '3': "relu1_2",
+            '8': "relu2_2",
+            '15': "relu3_3",
+            '22': "relu4_3"
+        }
+
+    def forward(self, x):
+        output = {}
+        for name, module in self.vgg_layers._modules.items():
+            x = module(x)
+            if name in self.layer_name_mapping:
+                output[self.layer_name_mapping[name]] = x
+        return LossOutput(**output)
