7576e0138b1c3870f14763983aa31f168b8adece
CeShine Lee <shuanck@gmail.com>
2018-06-21 22:10:49 +0800

    Clean up

--- fast_neural_style/utils.py
+++ fast_neural_style/utils.py
@@ -1,22 +1,5 @@
-import torch
 import numpy as np
-from PIL import Image
 from torchvision import transforms
-
-def load_image(filename, size=None, scale=None):
-    img = Image.open(filename)
-    if size is not None:
-        img = img.resize((size, size), Image.ANTIALIAS)
-    elif scale is not None:
-        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
-    return img
-
-
-def save_image(filename, data):
-    img = data.clone().clamp(0, 255).numpy()
-    img = img.transpose(1, 2, 0).astype("uint8")
-    img = Image.fromarray(img)
-    img.save(filename)
 
 
 def gram_matrix(y):
@@ -26,10 +9,12 @@ def gram_matrix(y):
     gram = features.bmm(features_t) / (ch * h * w)
     return gram
 
+
 def tensor_normalizer():
     return transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225])
+
 
 def recover_image(img):
     return (
@@ -40,10 +25,3 @@ def recover_image(img):
         ).transpose(0, 2, 3, 1) *
         255.
     ).clip(0, 255).astype(np.uint8)
-
-def normalize_batch(batch):
-    # normalize using imagenet mean and std
-    mean = batch.new_tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
-    std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
-    batch = batch.div_(255.0)
-    return (batch - mean) / std
